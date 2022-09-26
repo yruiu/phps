221af1a2316b7958fe2a734b3d1cbaa3717327ff
yruiu <46344823+yruiu@users.noreply.github.com>
2022-09-26 16:00:05 +0300

    Update dbsetings.php

--- dbsetings.php
+++ dbsetings.php
@@ -1,4 +1,4 @@
 <?php
 	require 'rb/rb.php';
-	R::setup( 'mysql:host=sql11.freemysqlhosting.net;dbname=sql11520427','sql11520427', 'aw3XNVlVmn' ); 
+	R::setup( 'mysql:host=sql11.freemysqlhosting.net;dbname=sql11522350','sql11522350', 'aw3XNVlVmn' ); 
 ?>
